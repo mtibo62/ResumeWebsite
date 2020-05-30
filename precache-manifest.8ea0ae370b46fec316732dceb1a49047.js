596c017771e1c24d565c9cc19a419c76f5e42503
mtibo62 <mark@DESKTOP-21O1F6B.localdomain>
2020-05-30 18:08:03 -0500

    Updates

---
+++ precache-manifest.8ea0ae370b46fec316732dceb1a49047.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584712aae1b3117824bfa1746859796d",
+    "url": "/ResumeWebsite/index.html"
+  },
+  {
+    "revision": "a32df9a740d4cf427dd7",
+    "url": "/ResumeWebsite/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "27da1f10f7463c6e445d",
+    "url": "/ResumeWebsite/static/css/main.9d4ed368.chunk.css"
+  },
+  {
+    "revision": "a32df9a740d4cf427dd7",
+    "url": "/ResumeWebsite/static/js/2.b3913f88.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ResumeWebsite/static/js/2.b3913f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27da1f10f7463c6e445d",
+    "url": "/ResumeWebsite/static/js/main.4a1e76fd.chunk.js"
+  },
+  {
+    "revision": "c90a35cc7ff89e307fb7",
+    "url": "/ResumeWebsite/static/js/runtime-main.3c16265d.js"
+  },
+  {
+    "revision": "64aaeb1642cfcfb42a075db1fe9994da",
+    "url": "/ResumeWebsite/static/media/Bootstrap_logo.64aaeb16.svg"
+  },
+  {
+    "revision": "8a25346f15fdb8c5bc73a7675e0c9c77",
+    "url": "/ResumeWebsite/static/media/LSU.8a25346f.svg"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/ResumeWebsite/static/media/React-icon.638be909.svg"
+  },
+  {
+    "revision": "c6961b79062b431cb39106d60635832d",
+    "url": "/ResumeWebsite/static/media/csharp.c6961b79.svg"
+  },
+  {
+    "revision": "a7c7f4799456736f441ebe1fb85353e2",
+    "url": "/ResumeWebsite/static/media/css.a7c7f479.svg"
+  },
+  {
+    "revision": "c215f432b8a33ff4d852beb52b2a1436",
+    "url": "/ResumeWebsite/static/media/filtered.c215f432.jpg"
+  },
+  {
+    "revision": "bc3e9619b024972d991057ad7ddedf37",
+    "url": "/ResumeWebsite/static/media/green-leaves.bc3e9619.jpg"
+  },
+  {
+    "revision": "32cd7df465dd5343163cf72ba0e3f8c8",
+    "url": "/ResumeWebsite/static/media/headshot.32cd7df4.jpg"
+  },
+  {
+    "revision": "a91ec9c4cfbbd12591964870f800c5af",
+    "url": "/ResumeWebsite/static/media/html.a91ec9c4.svg"
+  },
+  {
+    "revision": "a602fe1fb0c1cd24329e977873c1cc74",
+    "url": "/ResumeWebsite/static/media/java.a602fe1f.svg"
+  },
+  {
+    "revision": "79046a553ce2e9ea7fc1992ede20e610",
+    "url": "/ResumeWebsite/static/media/javascript.79046a55.png"
+  },
+  {
+    "revision": "ea4ed400406c9dbfcedbe204de531dc1",
+    "url": "/ResumeWebsite/static/media/javascript_logo.ea4ed400.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ResumeWebsite/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7eb80c46e2b5bcb52244e76027c046bf",
+    "url": "/ResumeWebsite/static/media/python.7eb80c46.svg"
+  },
+  {
+    "revision": "254e2aade1a20e80766f37f209f25845",
+    "url": "/ResumeWebsite/static/media/react.254e2aad.png"
+  },
+  {
+    "revision": "4f577019c3a0b894fcef8a925f06f953",
+    "url": "/ResumeWebsite/static/media/scenic-mount.4f577019.jpg"
+  }
+]);
